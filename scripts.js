bb4c1d46e667e994cfb4e6670a79681ae0d5963f
Alexy <formoza@autistici.org>
2016-06-13 15:11:49 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,37 @@
+function sticky_relocate() {
+    var window_top = $(window).scrollTop();
+    var div_top = $('#sticky-anchor').offset().top;
+    if (window_top > div_top) {
+        $('#sticky').addClass('stick');
+    } else {
+        $('#sticky').removeClass('stick');
+    }
+}
+
+$(function () {
+    $(window).scroll(sticky_relocate);
+    sticky_relocate();
+});
+
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 420) {
+        $('#scroller').css('top', $(window).scrollTop());
+    }
+}
+);
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 800);
+        return false;
+      }
+    }
+  });
+});
